9460873a47a01574f76405f7612abdad0c162d70
Sergio L. Benitez D <sergio.kun21@gmail.com>
2019-02-15 15:48:38 -0500

    vue: use vue cli to create the project- component: create first version of header component. docs: add tech stack and refs.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # bos-website
+
+## Web Tech-Stack
++ Vue
++ CSS3
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+### Refs
++ [Ashford-Ashford](http://ashford-ashford.com)
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="header">
+      <nav class="header__menu">
+        <img alt="Bos logo" src="../assets/logo.png">
+        <ul class="header__links">
+            <li><a href="#principles">Principios</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Conacto</a></li>
+        </ul>
+      </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.header__menu {
+    align-items: center;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.header__links {
+    flex-grow: 1;
+    padding-left: 50%;
+}
+
+</style>
