2dbd3613e482601cce407e364dffcd3a82ca4056
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-02-20 16:02:48 -0500

    component: working in quote component.

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,6 +1,8 @@
 <template>
     <section class="quote">
+        <div class="quote__square"></div>
         <h1 class="quote__header">¡Encuentra tu propia grandeza! <br/> Nosotros te apoyamos. </h1>
+        <div class="quote__square"></div>
     </section>
 </template>
 
@@ -9,3 +11,31 @@ export default {
   name: 'Quote',
 }
 </script>
+
+<style>
+.quote {
+  text-align: center;
+  margin: 32px 0  16px;
+}
+
+.quote__square {
+  background-image: url('../assets/quote_square.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 24px;
+  margin: 0 auto;
+  position: relative;
+  width: 24px;
+  z-index: 2;
+}
+
+.quote__header {
+  font-family: 'Fira Sans', sans-serif;
+  font-size: 64px;
+  font-style: italic;
+  font-weight: 900;
+  text-transform: uppercase;
+
+  margin: 24px 0;
+}
+</style>
