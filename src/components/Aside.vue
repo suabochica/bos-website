0afcee6cf0c84b2b0b974f22613f2d8bfa142b2b
Sergio Benítez <sergio.kun21@gmail.com>
2019-08-28 15:52:34 -0500

    component: start aside menu component.

---
+++ src/components/Aside.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="sidebar">
+    <ul class="sidebar-links">
+      <li>
+        <section class="sidebar-group">
+          <p class="sidebar-heading">
+            <span>{{ group_title }}</span>
+          </p>
+          <ul class="sidebar-links sidebar-group-items">
+            <li>
+              <a href class="sidebar-link">{{link_title}}</a>
+            </li>
+          </ul>
+        </section>
+      </li>
+    </ul>
+  </aside>
+</template>
